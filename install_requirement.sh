7dfe7e505ffd3884babecf2da479895271130aca
Ahmed Ada-Hanifi <ahmedada-hanifi@Ahmeds-MacBook-Pro.fritz.box>
2024-07-23 01:29:31 +0200

    updqte requirements

--- install_requirement.sh
+++ install_requirement.sh
@@ -1,14 +1,13 @@
 
 ### `install_requirements.sh`
 
-Here is the updated `install_requirements.sh` script to automate the installation process with conda:
+# Here is the updated `install_requirements.sh` script to automate the installation process with conda:
 
-```bash
 #!/bin/bash
 
 # From the directory /topic_matching_project it creates a conda environment
-conda create --name topic_matching_env python=3.9 -y
-source activate topic_matching_env
+conda create --name topic_matching_env2 python=3.9 -y
+source activate topic_matching_env2
 
 # Install dependencies
 pip install -r requirements.txt
