3545744f74a11b11748dd405c5bf8d46f4f53d4d
Ahmed Ada-Hanifi <ahmedada-hanifi@Ahmeds-MacBook-Pro.fritz.box>
2024-07-26 00:50:56 +0200

    forgotten file in the last commits

--- code/topic_matching/backend/flask_backend.py
+++ code/topic_matching/backend/flask_backend.py
@@ -15,11 +15,8 @@ def run_matching():
 @app.route('/run_new_matching', methods=['POST'])
 def run_new_matching():
     user_data = [request.json]
-    #print(type(user_data))
     content = load_content()
-    #print('user_data', user_data)
     matched_results = match_content(user_data, content)
-    #print('matched result', matched_results)
     return jsonify(matched_results)
 
 @app.route('/run_matching_without_threshold', methods=['POST'])
